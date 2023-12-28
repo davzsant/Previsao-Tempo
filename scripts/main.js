64f3c3f89084a1b1787280d96a26b5de96294f28
Millexs <david.lucas.santiago02@gmail.com>
2023-12-28 02:34:46 -0300

    Comeco da logica javascript
    Erro na hora de chamar a api

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,17 @@
 //const coisa = document.getElementbyId()
+const i_city = document.getElementById("i_city")
+const p_cidade = document.getElementById("cidade")
+const p_clima = document.getElementById("clima")
+const p_umidade = document.getElementById("umidade")
+const btn_find = document.getElementById("btn_find")
+
+const key = `f929daa369689e4adad381db8c5b583c`
+
+btn_find.addEventListener("click", async()=>{
+    const cidade = i_city.value
+    await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cidade}&appid=${key}`)
+    .then(res=>res.json())
+    .then(data=>{
+        console.log(data)
+    })
+})
